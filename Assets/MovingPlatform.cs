56a1a399070508f70f7460d67931a3f4cb60319a
Albert Wohletz <albert@wohletz.net>
2017-05-28 21:22:12 -0700

    Kill more tabs.

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
-	public Vector3 initialDestination = new Vector3(0,0,1.0f);
-	private Vector3 destination;
-	public float period = 3.0f;
-	public Vector3 startPosition;
-	private float speed;
+    public Vector3 initialDestination = new Vector3(0,0,1.0f);
+    private Vector3 destination;
+    public float period = 3.0f;
+    public Vector3 startPosition;
+    private float speed;
 
-	// Use this for initialization
-	void Start () {
-		destination = initialDestination;
-		startPosition = this.transform.localPosition;
-		speed = ((startPosition - destination).magnitude / period) * Time.deltaTime;
-		//Debug.Log ("speed: " + speed);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.localPosition = Vector3.MoveTowards (transform.localPosition, destination, speed);
-		if(transform.localPosition == destination){
-			SwitchDirection ();
-		}
+    // Use this for initialization
+    void Start () {
+        destination = initialDestination;
+        startPosition = this.transform.localPosition;
+        speed = ((startPosition - destination).magnitude / period) * Time.deltaTime;
+        //Debug.Log ("speed: " + speed);
+    }
 
-	}
+    // Update is called once per frame
+    void Update () {
+        transform.localPosition = Vector3.MoveTowards (transform.localPosition, destination, speed);
+        if(transform.localPosition == destination){
+            SwitchDirection ();
+        }
 
-	private void SwitchDirection(){
-		if (destination == startPosition) {
-			destination = initialDestination;
-		} else {
-			destination = startPosition;
-		}
-	}
+    }
+
+    private void SwitchDirection(){
+        if (destination == startPosition) {
+            destination = initialDestination;
+        } else {
+            destination = startPosition;
+        }
+    }
 }
