13f99feed9d9c35a3f7f4f2130746ae88663a05b
Thomas Beirne <thomas@buildingconnected.com>
2017-06-03 16:22:32 -0700

    Make the camera follow, kind of

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class MainCamera : MonoBehaviour {
     GameObject player;
+    float horizAngle = 180;
+    float vertAngle = 0.3f;
+    Vector3 offset;
+    float distance;
 
-	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
+        offset = player.transform.position - transform.position;
+        distance = offset.magnitude;
+        horizAngle = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         transform.LookAt(player.transform);
+		horizAngle += CrossPlatformInputManager.GetAxis("Mouse X") / 50f;
+		vertAngle -= CrossPlatformInputManager.GetAxis("Mouse Y") / 50f;
+        float y = Mathf.Sin(vertAngle) * distance;
+        float xyDist = Mathf.Cos(vertAngle) * distance;
+        float x = Mathf.Sin(horizAngle) * xyDist;
+        float z = Mathf.Cos(horizAngle) * xyDist;
+        transform.position = player.transform.position + new Vector3(x, y, z);
 	}
 }
