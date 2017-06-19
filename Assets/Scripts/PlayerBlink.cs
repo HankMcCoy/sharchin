e71ee01321bc19ed1724837b86c5425e31d12668
Steven Purn <smp5119@gmail.com>
2017-06-18 18:39:56 -0700

    Updated Blink Visual FX
    - Added tags to denote which objects cannot be blinked through
    - Drawing a raycast of where the player will be blinking

--- Assets/Scripts/PlayerBlink.cs
+++ Assets/Scripts/PlayerBlink.cs
@@ -3,30 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerBlink : MonoBehaviour {
+public class PlayerBlink : MonoBehaviour
+{
 
     public bool resetVelocityOnBlink;
+    public Vector3 blinkVelocity = new Vector3(0, 0, 50.0f);
+    public float timeStep = 0.2f;
 
     private bool CanBlink { get; set; }
     private bool m_Blink, m_Cancel;
     private bool prefabInstantiated;
     private bool blinkInitiated;
+    private int blinkLayer = 1 << 9;
     private GameObject blinkPrefab;
     private GameObject instantiatedBlinkPrefab;
     private Rigidbody rigidBody;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         CanBlink = true;
         blinkPrefab = (GameObject)Resources.Load("Prefabs/BlinkPrefab");
         rigidBody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		m_Blink = CrossPlatformInputManager.GetButtonDown("Blink");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Blink = CrossPlatformInputManager.GetButtonDown("Blink");
         m_Cancel = CrossPlatformInputManager.GetButtonDown("Cancel");
-	}
+    }
 
     private void FixedUpdate()
     {
@@ -35,34 +41,58 @@ public class PlayerBlink : MonoBehaviour {
             if (m_Cancel)
             {
                 ResetBlinkPrefab();
+            }else{
+                PlotTrajectory(transform.position, blinkVelocity, timeStep, 1.5f);
             }
         }
 
-        if(m_Blink)
+        if (m_Blink)
         {
             if (blinkInitiated)
             {
                 Vector3 destination = new Vector3(instantiatedBlinkPrefab.transform.position.x, instantiatedBlinkPrefab.transform.position.y + 1, instantiatedBlinkPrefab.transform.position.z);
-				ResetBlinkPrefab();
+                ResetBlinkPrefab();
                 if (resetVelocityOnBlink)
                 {
                     rigidBody.velocity = Vector3.zero;
                     rigidBody.angularVelocity = Vector3.zero;
                 }
                 transform.position = destination;
-			}else
+            }
+            else
             {
-                instantiatedBlinkPrefab = Instantiate(blinkPrefab, new Vector3(0,transform.position.y + 10,0), Quaternion.identity);
+                instantiatedBlinkPrefab = Instantiate(blinkPrefab, new Vector3(0, transform.position.y + 10, 0), Quaternion.identity);
                 prefabInstantiated = true;
-				blinkInitiated = true;
+                blinkInitiated = true;
             }
         }
     }
 
-    private void ResetBlinkPrefab(){
-		Destroy(instantiatedBlinkPrefab);
-		instantiatedBlinkPrefab = null;
-		prefabInstantiated = false;
-		blinkInitiated = false;
+    private void ResetBlinkPrefab()
+    {
+        Destroy(instantiatedBlinkPrefab);
+        instantiatedBlinkPrefab = null;
+        prefabInstantiated = false;
+        blinkInitiated = false;
     }
+
+	public void PlotTrajectory(Vector3 start, Vector3 startVelocity, float timestep, float maxTime)
+	{
+		Vector3 prev = start;
+		for (int i = 1; ; i++)
+		{
+			float t = timestep * i;
+			if (t > maxTime) break;
+			Vector3 pos = PlotTrajectoryAtTime(start, startVelocity, t);
+			if (Physics.Linecast(prev, pos, blinkLayer)) break;
+			Debug.DrawLine(prev, pos, Color.red);
+			prev = pos;
+		}
+	}
+
+	public Vector3 PlotTrajectoryAtTime(Vector3 start, Vector3 startVelocity, float time)
+	{
+        Debug.Log("Drawing something");
+		return start + startVelocity * time + Physics.gravity * time * time * 0.5f;
+	}
 }
